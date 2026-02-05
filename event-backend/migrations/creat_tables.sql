5fecb76778daa59a8aaf2e457c8f3314fe446fa2
hugodlrll <hugo.del49@gmail.com>
2026-02-05 09:07:16 +0100

    Changing the sql table install script

---
+++ event-backend/migrations/creat_tables.sql
@@ -0,0 +1,25 @@
+CREATE TABLE events (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(200) NOT NULL,
+    description TEXT,
+    date DATE NOT NULL,
+    created_by INT REFERENCES users(id) ON DELETE SET NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW(),
+    seats INT NOT NULL
+);
+
+CREATE TABLE user_events (
+    id SERIAL PRIMARY KEY,
+    event_id INT NOT NULL REFERENCES events(id) ON DELETE CASCADE,
+    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT NOW(),
+    UNIQUE(event_id, user_id)
+);
+
+CREATE TABLE users(  
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255)
+);
--- event-backend/migrations/create_user_events.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE user_events (
-    id SERIAL PRIMARY KEY,
-    event_id INT NOT NULL REFERENCES events(id) ON DELETE CASCADE,
-    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    created_at TIMESTAMP DEFAULT NOW(),
-    UNIQUE(event_id, user_id)
-);
